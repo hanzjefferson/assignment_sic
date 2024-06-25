a7c6eca697b42c30424e2a7d70b2eb9a849e6c90
Hanz Jefferson <varkhan245@gmail.com>
2024-06-25 08:29:06 +0700

    new

--- assignment2/main.py
+++ assignment2/main.py
@@ -1,17 +1,21 @@
 from models import AI4IModel
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
+import pandas
 
 test_size = 0.36
 
 model = AI4IModel()
 model.csv("datasets/ai4i2020.csv")
-model.preprocessing("Machine failure", ["UDI","Product ID","Type","TWF","HDF","PWF","OSF","RNF"])
+model.preprocessing()
 model.split(size=test_size,random_state=42)
 
 model.fit()
 
 target = model.get_fit().predict(model.get_test()[0])
+result = pandas.DataFrame(model.get_test()[0])
+result['Machine failure'] = target
+print(result.head())
 
 plt.scatter(range(int(test_size*len(model.x))), target)
 plt.title("Prediksi Kesalahan Mesin (Machine Failure)")
